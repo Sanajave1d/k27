0a19aac6f95f203c8c3d7d20d0de0b009b301704
Sanajav1d <khawerkhan295@gmail.com>
2026-06-20 20:58:36 +0500

    fix images changes

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -32,7 +32,7 @@ const ProjectsCard = (props) => {
             <div
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
-                className='lg:w-1/2 lg:h-full relative project rounded-none hover:cursor-pointer hover:rounded-[50px] duration-500 transition-all ease-linear overflow-hidden w-full h-[32vh] bg-green-300' >
+                className='lg:w-1/2 lg:h-full relative project rounded-none hover:cursor-pointer hover:rounded-[50px] duration-500 transition-all ease-linear overflow-hidden w-full h-[32vh] ' >
                 <img className=' w-full h-full object-cover ' src={props.image1} alt="project 1" />
                 <div style={{ display: 'none', opacity: 0, transform: 'translateY(-40px)' }} className='overlay absolute top-1/2 left-0 -translate-y-1/2  p-6 w-full h-full' >
                     <h2 className='font-[font2] text-[4.5vw] text-center text-white pt-4   border-2 rounded-[50px] leading-[0.8]' >VIEW THE PROJECT</h2>
@@ -40,7 +40,7 @@ const ProjectsCard = (props) => {
             </div>
             <div onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
-                className='lg:h-full lg:w-1/2 relative project rounded-none hover:cursor-pointer hover:rounded-[50px] duration-500 transition-all ease-linear overflow-hidden w-full h-[32vh] bg-green-300' >
+                className='lg:h-full lg:w-1/2 relative project rounded-none hover:cursor-pointer hover:rounded-[50px] duration-500 transition-all ease-linear overflow-hidden w-full h-[32vh] ' >
                 <img className=' w-full h-full object-cover ' src={props.image2} alt="project 2" />
                 <div style={{ display: 'none', opacity: 0, transform: 'translateY(-40px)' }} className='overlay absolute top-1/2 left-0 -translate-y-1/2  p-6 w-full h-full' >
                     <h2 className='font-[font2] text-[4.5vw] text-center text-white pt-4   border-2 rounded-[50px] leading-[0.8]' >VIEW THE PROJECT</h2>
